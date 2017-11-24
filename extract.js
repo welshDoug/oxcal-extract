67657501abfaaeb53e5fb3a834947b7614c95b8f
Doug <Cowie>
2017-11-24 09:59:50 +0000

    extract means

--- extract.js
+++ extract.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+ocd = new Array(); 
+calib = new Array(); 
+model = new Object();
+require('./Hhill2.js');
+
+console.log("OCD Length: " + ocd.length);
+/*console.log("OCD[14].length: " + ocd[14].posterior.prob.length);
+console.log("OCD[14].start: " + ocd[14].posterior.start);
+console.log("OCD[14].end: " + (ocd[14].posterior.start+length));
+console.log("OCD[14].mean: " + ocd[14].posterior.mean);
+console.log("OCD[14].median: " + ocd[14].posterior.median);*/
+
+var means = {}
+for (let i in ocd) {
+ if (ocd[i].posterior && ocd[i].posterior.mean) {
+ means[ocd[i].name] = ocd[i].posterior.mean;
+ }
+} 
+
+console.log(means);
+
+//TODO convert to CSV
+
+//TODO write csv file
